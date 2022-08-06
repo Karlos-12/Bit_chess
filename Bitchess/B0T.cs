f0c6041c0ef541b381777f8e262d6e5c7d587500
Karel Cveček <kastrolekkaja@seznam.cz>
2022-08-06 12:30:12 +0200

    dgf

--- Bitchess/B0T.cs
+++ Bitchess/B0T.cs
@@ -22,17 +22,46 @@ namespace Bitchess
 {
     internal class bot
     {
+        public Side botside;
+        public List<bot_fig> all;
+
+        public bot(Side botside, List<bot_fig> all)
+        {
+            this.botside = botside;
+            this.all = all;
+        }
 
     }
 
     internal class Tah
     {
+        public List<bot_fig> enfigs;
+        public List<bot_fig> myfigs;
 
+        public Side botside;
+
+        public Tah(List<bot_fig> all, Side botside)
+        {
+            this.botside = botside;
+
+            for (int i = all.Count - 1; i >= 0; i--)
+            {
+                if (all[i].side == botside)
+                {
+                    myfigs.Add(all[i]);
+                }
+                else
+                {
+                    enfigs.Add(all[i]);
+                }
+            }
+         
+        }
     }
 
    internal class bot_fig
    {
-
+        public Side side;
    }
 
 }
